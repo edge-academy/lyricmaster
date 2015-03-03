57e29bd97e50e7bd3c05e733aad98666774a45d9
gadeweever <gamaldeweever@gmail.com>
2015-03-03 00:48:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lyricmaster::Application.config.secret_key_base = '00a78827f70650238ea987642b345eaf23d6a65517ad2aa2eac0906ed37678a596237b4626c9091e7d75ac45816678b2ef018d90a63209a5819377a7234cf7f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lyricmaster::Application.config.session_store :cookie_store, key: '_lyricmaster_session'
